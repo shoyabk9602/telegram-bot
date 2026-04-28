1809483f9b05276ba5ffd9eaa669c326e073eb9e
shoyabk9602 <56260656+shoyabk9602@users.noreply.github.com>
2026-04-28 22:36:45 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,36 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-BOT_TOKEN = os.getenv("8640066413:AAEjpnv1DMFsux3mhGkT6EoS1-_zY51uz8A")
+# 👉 Yaha apna FULL token daal (BotFather wala)
+BOT_TOKEN = "8640066413:AAEjpnv1DMFsux3mhGkT6EoS1-_zY51uz8A"
+
+# 👉 Tera channel username
 CHANNEL_ID = "@ikminvite"
 
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
+        # unique 1-user invite link
         link = await context.bot.create_chat_invite_link(
             chat_id=CHANNEL_ID,
             member_limit=1
         )
-        await update.message.reply_text(f"Join karo 👇\n{link.invite_link}")
+
+        await update.message.reply_text(
+            f"Join karo 👇\n{link.invite_link}"
+        )
+
     except Exception as e:
-        print(e)
+        print("Error:", e)
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply)
+    )
 
-print("Bot chal raha hai...")
-app.run_polling()
+    print("Bot chal raha hai...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
