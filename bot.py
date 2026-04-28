8e8c5bbd08a79691cb96d0eb78f781705fccd282
shoyabk9602 <56260656+shoyabk9602@users.noreply.github.com>
2026-04-28 21:57:15 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,23 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-BOT_TOKEN = "8671275232:AAGP3NtXvIuH16Sb28mrMOa3svKQuijVnfs"
+BOT_TOKEN = os.getenv("8671275232:AAGP3NtXvIuH16Sb28mrMOa3svKQuijVnfs")
 CHANNEL_ID = "@ikminvite"
 
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    link = await context.bot.create_chat_invite_link(
-        chat_id=CHANNEL_ID,
-        member_limit=1
-    )
-    await update.message.reply_text(f"Join karo 👇\n{link.invite_link}")
+    try:
+        link = await context.bot.create_chat_invite_link(
+            chat_id=CHANNEL_ID,
+            member_limit=1
+        )
+        await update.message.reply_text(f"Join karo 👇\n{link.invite_link}")
+    except Exception as e:
+        print(e)
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
+
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
 
+print("Bot chal raha hai...")
 app.run_polling()
